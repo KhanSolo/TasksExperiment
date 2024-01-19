3e443684a6f0b36e9eba9d12ddc5aa6ccbcbc279
Konstantin <drpdrp@mail.ru>
2024-01-20 01:46:02 +0300

    ADD: ConfigManager

---
+++ ConfigManagerApp/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Configuration;
+using System.Xml;
+
+namespace ConfigManagerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var conf = new EntraBankConfiguration();
+            var pair = ConfigurationManager.GetSection("entraBank");
+
+            Console.WriteLine("");
+        }
+    }
+
+    [ConfigurationCollection(typeof(KeyValueConfigurationElement))]
+    public class EntraBankConfiguration : IConfigurationSectionHandler
+    {
+        public string InputChannel { get; set; }
+        public string MQPort { get; set; }
+
+        public object Create(object parent, object configContext, XmlNode section)
+        {
+            var result = new EntraBankConfiguration();
+
+            foreach (XmlNode node in section.ChildNodes)
+            {
+                var key = node.Attributes["key"].Value;
+                var value = node.Attributes["value"].Value;
+
+                if (key == "InputChannel")
+                {
+                    result.InputChannel = value;
+                }
+                else if (key == "MQPort")
+                {
+                    result.MQPort = value;
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ ConfigUtil/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConfigUtil
+{
+    public static class Program
+    {
+        public static async Task Main()
+        {
+            await DoSomething(1, async Task (n) => { await Task.Delay(n); });
+            //await DoSomething(2, async (n) => { await Task.Delay(n); });
+            //await DoSomething(3, async (n) => { await Task.Delay(n); });
+        }
+
+        private static async Task DoSomething(int num, Func<int, Task> func)
+        {
+            Log("Starting", num);
+            await func(num);
+            Log("Completing", num);
+        }
+
+        private static void Log(string action, int num)
+        {
+            Console.WriteLine($"{action} {num}, threadId {Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+}
