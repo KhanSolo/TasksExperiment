f9ac7900821308e8c76490b3c7eff886d1fca2dd
Konstantin <drpdrp@mail.ru>
2024-01-20 01:46:36 +0300

    ADD: Iterators

---
+++ Iterators/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Iterators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var i in Fib())
+            {
+                if (i > 100) break;
+                Console.WriteLine(i);
+            }
+        }
+
+        public static IEnumerable<int> Fib()
+        {
+            Console.WriteLine("Enter");
+            int prev = 0, next = 1;
+            yield return prev;
+            yield return next;
+
+            while (true)
+            {
+                int sum = prev + next;
+                yield return sum;
+                prev = next;
+                next = sum;
+            }
+        }
+    }
+}
